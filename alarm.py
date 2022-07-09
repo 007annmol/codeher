361fd33f9436c9b42661378a195227dd4c9fd9be
Ann Mary Vincent <annusmaryvincent@gmail.com>
2022-07-09 23:33:45 +0530

    shortlist task

---
+++ alarm.py
@@ -0,0 +1,17 @@
+import time,winsound,datetime
+
+print('enter the time in hh:mm')
+almtim=input()
+currenttime=time.strftime("%H:%M")
+print(f"the alarm time is {almtim}")
+while almtim!=currenttime:
+    currenttime=time.strftime("%H:%M")
+    time.sleep(10)
+print("current   time:",currenttime)
+print("Alarm set time:",almtim)
+if almtim==currenttime:
+    print("playing alarm")
+    winsound.PlaySound("*",winsound.SND_ASYNC)
+    print("alarm sound playing")
+
+#code working
